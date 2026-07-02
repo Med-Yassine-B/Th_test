95d3a9123ce52f82c82794cc042920ea9bce507d
Med-Yassine-B <bouzidimouhamedyessin@gmail.com>
2026-07-02 22:43:47 +0100

    test:adding test examples

---
+++ test_example.cpp
@@ -0,0 +1,24 @@
+#include "th_test.hpp"
+
+
+int add(int a,int b){
+    return a+b;
+}
+
+void test_add(){
+    TH_TEST::assert(add(5,5)==10);
+    TH_TEST::assert(add(5,3)==10);
+    TH_TEST::assert(add(5,2)==10);
+    TH_TEST::assert(add(5,9)==10);
+}
+
+TEST(test_add);
+
+int main(){
+    
+    TH_TEST::RUN_TESTS();
+
+    return 0;
+}
+
+
